68697ef74ca2f593d26e854235dcb11c51a60fca
Fauzan Rahmatullah <fauzanrahmatullah@pop-os.localdomain>
2020-04-02 20:42:03 +0700

    add google oauth

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 router.post('/signup', userController.signup)
-
 router.post('/signin', userController.signin)
+router.post('/googleSign', userController.googleSign)
 
 module.exports = router
