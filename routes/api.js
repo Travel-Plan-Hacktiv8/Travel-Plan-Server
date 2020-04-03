af4d68524d72cd7c85b75b9590f1552a52286276
ditobprasetio <ditobagus28@gmail.com>
2020-04-03 12:08:00 +0700

    holidayApi

--- routes/api.js
+++ routes/api.js
@@ -4,4 +4,5 @@ const apiController= require('../controllers/apiController')
 
 router.get('/news/:country', apiController.findContentNews)
 router.get('/weather/:city', apiController.findContentWeather)
+router.get('/holiday', apiController.holidayDate)
 module.exports = router
