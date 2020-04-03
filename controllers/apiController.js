8aa0e6119121eb6bbf181e196a3f060ec46c1faf
Fauzan Rahmatullah <fauzanrahmatullah@pop-os.localdomain>
2020-04-03 08:04:28 +0700

    pisah route news

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,27 +1,28 @@
 const axios = require('axios')
 
 class ApiController {
-    static findContent(req, res){
+    static findContentWeather(req, res){
         const city = req.params.city
         console.log(city)
-        let weather = ''
-        let detikNews = ''
         axios.get(`https://api.weatherbit.io/v2.0/current?city=${city}&key=${process.env.APIKEY}`)
             .then(result => {
                 const {data} = result
-                // console.log(data.data[0].country_code)
-                weather = data
-                return axios.get(`http://newsapi.org/v2/top-headlines?country=${data.data[0].country_code}&category=general&apiKey=${process.env.APIKEYNEWS}`)
-            })
-            .then(result => {
-                const {data} = result
-                detikNews = data
                 return res.status(200).json({
-                    Weather: weather,
-                    News: detikNews
+                    data
                 })
             })
             .catch(err => res.status(500).json(err))
+    }
+    static findContentNews(req, res) {
+        const country = req.params.country
+        axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&category=general&apiKey=${process.env.APIKEYNEWS}`)
+        .then(result => {
+            const {data} = result
+            return res.status(200).json({
+               data
+            })
+        })
+        .catch(err => res.status(500).json(err))
     }
         
 }
--- routes/api.js
+++ routes/api.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 const apiController= require('../controllers/apiController')
 
-router.get('/:city', apiController.findContent)
+router.get('/:city', apiController.findContentWeather)
+router.get('/:country', apiController.findContentNews)
 module.exports = router
